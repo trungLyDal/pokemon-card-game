7d8cb5934ffeb957dc3077777203d7caf661fc44
Trung Ly <Jun.Ly@dal.ca>
2025-05-25 20:13:32 -0300

    commit

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ try {
 
 // MongoDB connection
 const MONGO_URI = process.env.MONGO_URI;
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
